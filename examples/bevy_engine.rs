b57078e27a9478801565efb7f2e47c268b0f14dd
Blake Ellis <blake@commercev3.com>
2020-12-23 08:02:43 -0500

    added tile flipping

--- examples/bevy_engine.rs
+++ examples/bevy_engine.rs
@@ -4,6 +4,14 @@ use ldtk_rust::LdtkFile;
 
 use std::collections::HashMap;
 
+// extend the LdtkFile struct with whatever you need for your
+// game engine.
+struct Map {
+    ldtk_file: LdtkFile,
+    redraw: bool,
+    current_level: usize,
+}
+
 fn main() {
     App::build()
         .add_resource(WindowDescriptor {
@@ -23,14 +31,15 @@ fn setup(
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
     // load & parse the LDtk JSON file
-    let mut ldtk = LdtkFile::new("assets/AutoLayers_4_Advanced.ldtk".to_string());
-
-    // the redraw field gives us some control on when to spawn
-    ldtk.redraw = true;
+    let map = Map {
+        ldtk_file: LdtkFile::new("assets/AutoLayers_4_Advanced.ldtk".to_string()),
+        redraw: true,
+        current_level: 0,
+    };
 
     // now set up the tile assets
     let mut texture_atlas_handles: HashMap<i32, Handle<TextureAtlas>> = HashMap::new();
-    for tileset in ldtk.defs.tilesets.iter() {
+    for tileset in map.ldtk_file.defs.tilesets.iter() {
         // load the asset
         let texture_handle = asset_server.load(&tileset.rel_path[..]);
 
@@ -50,54 +59,69 @@ fn setup(
 
     // add the LDtk object and the tile assets as resources
     commands
-        .insert_resource(ldtk)
+        .insert_resource(map)
         .insert_resource(texture_atlas_handles);
 }
 
 fn update(
     commands: &mut Commands,
-    mut ldtk: ResMut<LdtkFile>,
+    mut map: ResMut<Map>,
     handles: Res<HashMap<i32, Handle<TextureAtlas>>>,
 ) {
-    if !ldtk.redraw {
+    if !map.redraw {
         return;
     }
 
     let tile_scale = 2.5;
+
     commands.spawn(Camera2dBundle::default());
-    for level in ldtk.levels.iter() {
-        commands.insert_resource(ClearColor(Color::hex(&level.__bg_color[1..]).unwrap()));
-        for layer in level.layer_instances.iter() {
-            if layer.__type == "IntGrid" || layer.__type == "AutoLayer" {
-                let tileset_uid = layer.__tileset_def_uid.unwrap_or(-1);
-                if tileset_uid >= 0 {
-                    let layer_width =
-                        layer.__c_wid as f32 * (layer.__grid_size as f32 * tile_scale);
-                    let layer_height =
-                        layer.__c_hei as f32 * (layer.__grid_size as f32 * tile_scale);
-                    for tile in layer.auto_layer_tiles.iter() {
-                        commands.spawn(SpriteSheetBundle {
-                            transform: Transform {
-                                translation: convert_to_world(
-                                    layer_width,
-                                    layer_height,
-                                    tile_scale,
-                                    tile.px[0],
-                                    tile.px[1],
-                                ),
-                                scale: Vec3::splat(tile_scale),
-                                ..Default::default()
-                            },
-                            sprite: TextureAtlasSprite::new(tile.t as u32),
-                            texture_atlas: handles[&tileset_uid].clone(),
-                            ..Default::default()
-                        });
+    //for level in map.ldtk_file.levels.iter() {
+    commands.insert_resource(ClearColor(
+        Color::hex(&map.ldtk_file.levels[0].__bg_color[1..]).unwrap(),
+    ));
+    for layer in map.ldtk_file.levels[map.current_level]
+        .layer_instances
+        .iter()
+    {
+        if layer.__type == "IntGrid" || layer.__type == "AutoLayer" {
+            let tileset_uid = layer.__tileset_def_uid.unwrap_or(-1);
+            if tileset_uid >= 0 {
+                let layer_width = layer.__c_wid as f32 * (layer.__grid_size as f32 * tile_scale);
+                let layer_height = layer.__c_hei as f32 * (layer.__grid_size as f32 * tile_scale);
+                for tile in layer.auto_layer_tiles.iter() {
+                    let mut flip_x = false;
+                    let mut flip_y = false;
+                    match tile.f {
+                        1 => flip_x = true,
+                        2 => flip_y = true,
+                        3 => {
+                            flip_x = true;
+                            flip_y = true
+                        }
+                        _ => (),
                     }
+                    commands.spawn(SpriteSheetBundle {
+                        transform: Transform {
+                            translation: convert_to_world(
+                                layer_width,
+                                layer_height,
+                                tile_scale,
+                                tile.px[0],
+                                tile.px[1],
+                            ),
+                            rotation: flip(flip_x, flip_y),
+                            scale: Vec3::splat(tile_scale),
+                        },
+                        sprite: TextureAtlasSprite::new(tile.t as u32),
+                        texture_atlas: handles[&tileset_uid].clone(),
+                        ..Default::default()
+                    });
                 }
             }
         }
     }
-    ldtk.redraw = false;
+    //}
+    map.redraw = false;
 }
 
 // LDtk provides pixel locations starting in the top left. For Bevy we need to
@@ -106,4 +130,17 @@ fn convert_to_world(width: f32, height: f32, scale: f32, x: i32, y: i32) -> Vec3
     let world_x = (x as f32 * scale) - (width / 2.);
     let world_y = -(y as f32 * scale) + (height / 2.);
     Vec3::new(world_x, world_y, 0.)
+}
+
+// if tile needs to flip on either axis, flip it
+fn flip(x: bool, y: bool) -> Quat {
+    let mut q1 = Quat::default();
+    let mut q2 = Quat::default();
+    if x {
+        q1 = Quat::from_rotation_y(std::f32::consts::PI);
+    }
+    if y {
+        q2 = Quat::from_rotation_x(std::f32::consts::PI);
+    }
+    q1 * q2
 }
