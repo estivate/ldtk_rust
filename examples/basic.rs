f95d43505d1533363331f38fae070dea5627f5b0
Blake <blake@commercev3.com>
2021-03-03 08:26:07 -0500

    updating to 0.8.1

--- examples/basic.rs
+++ examples/basic.rs
@@ -2,7 +2,7 @@ use ldtk_rust::Project;
 // Loads an LDtk Project file along with any external level files
 // that it references.
 fn main() {
-    let file_path = "assets/game_0-7-1.ldtk".to_string();
+    let file_path = "assets/game_0-8-1.ldtk".to_string();
     let project: Project = Project::new(file_path);
     
     println!("First level pixel height is {}!", project.levels[0].px_hei);
