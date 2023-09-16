2b81694e501ff11ae5e31fa68d61a09ffe5a02a9
Patryk 'PsichiX' Budzynski <psichix@gmail.com>
2023-09-16 04:33:41 +0200

    disable bevy example that fails tests

--- examples/bevy_engine.rs
+++ examples/bevy_engine.rs
@@ -1,25 +1,25 @@
-use bevy::prelude::*;
-
 // example rewrite in progress, I'm not using Bevy so welcome
 // assist here.
-//
+
+// use bevy::prelude::*;
+
 //const LDTK_FILE_PATH: &str = "assets/game_1-1-3.ldtk";
 
 fn main() {
-    App::new()
-        .insert_resource(WindowDescriptor {
-            title: "title".to_string(),
-            width: 1024.0,
-            height: 768.0,
-            ..Default::default()
-        })
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(setup_camera)
-        .run();
+//     App::new()
+//         .insert_resource(WindowDescriptor {
+//             title: "title".to_string(),
+//             width: 1024.0,
+//             height: 768.0,
+//             ..Default::default()
+//         })
+//         .add_plugins(DefaultPlugins)
+//         .add_startup_system(setup_camera)
+//         .run();
 }
 
-fn setup_camera(mut commands: Commands) {
-    let mut camera = OrthographicCameraBundle::new_2d();
-    camera.transform = Transform::from_translation(Vec3::new(0.0, 0.0, 50.0));
-    commands.spawn_bundle(camera);
-}
+// fn setup_camera(mut commands: Commands) {
+//     let mut camera = OrthographicCameraBundle::new_2d();
+//     camera.transform = Transform::from_translation(Vec3::new(0.0, 0.0, 50.0));
+//     commands.spawn_bundle(camera);
+// }
